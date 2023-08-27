93f95a04375f2d775c4e12e363c563e42c8ad5e9
Matt Drago <matt@drago.com.au>
2023-08-28 06:37:48 +1000

    Adds a basic Header Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Header } from '../components/Header'
+
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { Cinzel_Decorative } from "next/font/google";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCog } from "@fortawesome/free-solid-svg-icons";
+
+import Image from "next/image";
+
+const cinzel = Cinzel_Decorative({
+  weight: "900",
+  style: "normal",
+  subsets: ["latin"],
+});
+
+export function Header() {
+  return (
+    <div id="header" className="flex flow-row pb-4 pt-2 px-2">
+      <h1
+        className={`${cinzel.className} small-caps md:text-8xl text-7xl md:pl-8 pl-4 grow`}
+      >
+        Libre Mei
+      </h1>
+      <Image
+        width={96}
+        height={96}
+        src="/blank-profile-picture-973460_640.png"
+        alt="profile image"
+        className="md:w-24 md:h-24 w-16 h-16 rounded-full md:mr-4 mr-2"
+      />
+      <FontAwesomeIcon icon={faCog} />
+    </div>
+  );
+}
