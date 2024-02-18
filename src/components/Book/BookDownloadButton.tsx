85f51ff31b1e8880f484b336493116ad78a5b30b
Matt Drago <matt@drago.com.au>
2024-02-18 22:12:19 +1100

    create a custom icon for ePub files

--- src/components/Book/BookDownloadButton.tsx
+++ src/components/Book/BookDownloadButton.tsx
@@ -2,25 +2,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFileCode,
   faFilePdf,
-  faFileText,
   faFileWord,
 } from "@fortawesome/free-solid-svg-icons";
+
+import { libreIconsEpubFile } from "./types";
 
 import { BookEdition } from "./types";
 
 export function BookDownloadButton({ format, url }: BookEdition) {
-    const formatToIcon = {
-      azw: faFileCode,
-      epub: faFileText,
-      mobi: faFileWord,
-      pdf: faFilePdf,
-    };
-  
-    return (
-      <a title={`Download ${format}`} href={`/download${url}`}>
-        <FontAwesomeIcon icon={formatToIcon[format]} className="text-3xl"/>
-      </a>
-    );
-  }
-  
-  
+  const formatToIcon = {
+    azw: faFileCode,
+    epub: libreIconsEpubFile,
+    mobi: faFileWord,
+    pdf: faFilePdf,
+  };
+
+  return (
+    <a title={`Download ${format}`} href={`/download${url}`}>
+      <FontAwesomeIcon icon={formatToIcon[format]} className="text-3xl" />
+    </a>
+  );
+}
--- src/components/Book/types.tsx
+++ src/components/Book/types.tsx
@@ -1,25 +1,41 @@
+import {
+  IconDefinition,
+  IconName,
+  IconPrefix,
+} from "@fortawesome/fontawesome-svg-core";
+
 export interface BookCover {
-    imageSrc: string;
-    width: number;
-    height: number;
-  }
-  
-  type BookFormat = "azw" | "epub" | "mobi" | "pdf";
-  
-  export interface BookEdition {
-    format: BookFormat;
-    url: string;
-  }
-  
-  export interface BookDetails {
-    id: string;
-    title: string;
-    author: string;
-    description?: string;
-    publisher?: string;
-    coverImage: BookCover;
-    editions: BookEdition[];
-    shelves: string[];
-  }
-  
-  
+  imageSrc: string;
+  width: number;
+  height: number;
+}
+
+type BookFormat = "azw" | "epub" | "mobi" | "pdf";
+
+export interface BookEdition {
+  format: BookFormat;
+  url: string;
+}
+
+export interface BookDetails {
+  id: string;
+  title: string;
+  author: string;
+  description?: string;
+  publisher?: string;
+  coverImage: BookCover;
+  editions: BookEdition[];
+  shelves: string[];
+}
+
+export const libreIconsEpubFile: IconDefinition = {
+  icon: [
+    544, // width
+    544, // height
+    [], // ligatures
+    "", // unicode
+    "m269.4 438.74-169.3-169.34 169.3-169.3 56.46 56.43-112.9 112.87 56.45 56.45 169.33-169.3-147.5-147.5c-12.04-12.06-31.58-12.06-43.63 0l-238.58 238.55c-12.03 12.06-12.03 31.6 0 43.65l238.57 238.55c12.05 12.05 31.6 12.05 43.63 0l238.57-238.56c12.04-12.05 12.04-31.6 0-43.63l-34.64-34.61z", // svgPathData
+  ],
+  iconName: "libre-icons-epub-file" as IconName,
+  prefix: "libre-icons" as IconPrefix,
+};
