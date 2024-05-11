48c73ac71daa5902abf7955bb497d0a0c1e0ffc8
Matt Drago <matt@drago.com.au>
2024-05-11 14:33:19 +1000

    Add Button to activate edit mode of subjects

--- src/components/Book/BookSubjects.tsx
+++ src/components/Book/BookSubjects.tsx
@@ -1,8 +1,13 @@
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
 
 export function BookSubjectBar({subjects} : {subjects : string[]}) {
+    function startEditMode() {}
 
-    return <div className="flex flex-wrap justify-start gap-2">
+    return <div className="flex flex-wrap justify-start gap-2 relative">
+      <div className="cursor-pointer absolute top-0 right-1 text-xl" onClick={startEditMode}>
+        <FontAwesomeIcon icon={faPencil} className="absolute top-0 right-0" />
+      </div>
         {subjects.map((subject) => {
             return <span key={subject} className="text-sm px-1 border rounded-lg">{subject}</span>;
         })}
