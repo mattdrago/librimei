7db5b65cb29a7b5d4619f62e2d1d54b6e8ceba55
Matt Drago <matt@drago.com.au>
2024-06-08 09:31:46 +1000

    displays error message on book load or save failure

--- src/components/Book/BookFullDetailsMenu.tsx
+++ src/components/Book/BookFullDetailsMenu.tsx
@@ -6,30 +6,34 @@ import {
   faSave,
   faCancel,
 } from "@fortawesome/free-solid-svg-icons";
+import { BookAPIError, BookAPIOperationType } from "@/services/BookAPI";
 
 interface BookFullDetailsMenuProps {
-    editMode : boolean;
-    onClose : Function;
-    onStartEditing : Function;
-    onSave : Function;
-    onCancelEditing : Function;
-  }
-  
-  export function BookFullDetailsMenu({editMode, onClose, onStartEditing, onSave, onCancelEditing} : BookFullDetailsMenuProps) {
-  
-    return <>
-            <div className="pt-1 px-2 text-xl max-w-8 min-w-8">
-            <FontAwesomeIcon
-              icon={faClose}
-              className="cursor-pointer hover:scale-125 transition ease-in-out"
-              onClick={() => { onClose() }}
-            />
+  editMode: boolean;
+  apiError: BookAPIError | undefined;
+  onClose: Function;
+  onStartEditing: Function;
+  onSave: Function;
+  onCancelEditing: Function;
+}
+
+export function BookFullDetailsMenu({editMode, apiError, onClose, onStartEditing, onSave, onCancelEditing} : BookFullDetailsMenuProps) {
+  return (
+    <>
+      <div className="pt-1 px-2 text-xl max-w-8 min-w-8">
+        <FontAwesomeIcon
+          icon={faClose}
+          className="cursor-pointer hover:scale-125 transition ease-in-out"
+          onClick={() => { onClose(); }}
+        />
+        {apiError?.operationType != BookAPIOperationType.LOAD && (
+          <>
             <FontAwesomeIcon icon={faMinus} className="text-gray-400" />
             {!editMode && (
               <FontAwesomeIcon
                 icon={faPencil}
                 className="cursor-pointer hover:scale-125 transition ease-in-out"
-                onClick={() => { onStartEditing()}}
+                onClick={() => { onStartEditing(); }}
               />
             )}
             {editMode && (
@@ -37,15 +41,18 @@ interface BookFullDetailsMenuProps {
                 <FontAwesomeIcon
                   icon={faSave}
                   className="cursor-pointer hover:scale-125 transition ease-in-out"
-                  onClick={() => {onSave()}}
+                  onClick={() => { onSave(); }}
                 />
                 <FontAwesomeIcon
                   icon={faCancel}
                   className="cursor-pointer hover:scale-125 transition ease-in-out"
-                  onClick={() => {onCancelEditing()}}
+                  onClick={() => { onCancelEditing(); }}
                 />
               </>
             )}
-          </div>
-    </>;
-  }
+          </>
+        )}
+      </div>
+    </>
+  );
+}
--- src/services/BookAPI.tsx
+++ src/services/BookAPI.tsx
@@ -1,28 +1,63 @@
 import { BookDetails } from "@/components/Book/types";
 
-class BookNotFoundError extends Error {
-    id: string;
-    
-    constructor(id: string) {
-        super(`Book ${id} was not found`);
-        this.name = "BookNotFoundError";
-        this.id = id;
-    }
+export class BookAPIError extends Error {
+  bookId: string;
+  message: string;
+  operationType: BookAPIOperationType;
+
+  constructor(bookId: string, message: string, operationType: BookAPIOperationType) {
+    super(message);
+
+    this.bookId = bookId;
+    this.message = message;
+    this.operationType = operationType;
+  }
 }
 
+export const enum BookAPIOperationType {
+  LOAD,
+  SAVE,
+}
 
-async function loadBook(id: string) : Promise<BookDetails> {
-    var response = await fetch(`/book/${id}`);
+class BookNotFoundError extends BookAPIError {
+  constructor(bookId: string) {
+    super(bookId, `Book ${bookId} was not found`, BookAPIOperationType.LOAD);
+  }
+}
 
-    if(response.ok) {
-        return await response.json();
-    } else if (response.status == 404) {
-        throw new BookNotFoundError(id);
-    } else {
-        throw new Error(`Error occured whilst calling Book API: ${response.status} - ${response.statusText}`);
-    }
+async function loadBook(id: string): Promise<BookDetails> {
+  var response = await fetch(`/book/${id}`);
+
+  if (response.ok) {
+    return await response.json();
+  } else if (response.status == 404) {
+    throw new BookNotFoundError(id);
+  } else {
+    throw new BookAPIError(
+      id,
+      `Error occured whilst loading Book ${id}: ${response.status} - ${response.statusText}`,
+      BookAPIOperationType.LOAD
+    );
+  }
+}
+
+async function saveBook(book: BookDetails) {
+  var response = await fetch(`/book/${book.id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(book),
+  });
+
+  if (!response.ok) {
+    throw new BookAPIError(
+      book.id,
+      `Error occured whilst saving Book ${book.id}: ${response.status} - ${response.statusText}`,
+      BookAPIOperationType.SAVE
+    );
+  }
 }
 
 export var bookAPI = {
-    'loadBook': loadBook,
-}
+  loadBook: loadBook,
+  saveBook: saveBook,
+};
