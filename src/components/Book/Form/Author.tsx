71be61d7b37f55dc38ef28ae06e82d30649b5dfb
Matt Drago <matt@drago.com.au>
2024-09-29 21:37:18 +1000

    Add Book: fix sorting of author
    Along with fixing the sorting of author input, the remove functionality was simplified and other components that were not needed were also removed.

--- src/components/Book/Form/Author.tsx
+++ src/components/Book/Form/Author.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import CreateableSelect from 'react-select/creatable'
-import { components, MultiValueGenericProps, ValueContainerProps, MultiValueRemoveProps } from 'react-select';
+import { components, ValueContainerProps, MultiValueRemoveProps, MultiValueProps, GroupBase } from 'react-select';
 import { DndContext, useSensors, useSensor, PointerSensor, KeyboardSensor, closestCenter, DragEndEvent } from '@dnd-kit/core';
 import { arrayMove, SortableContext, useSortable, sortableKeyboardCoordinates } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
@@ -38,43 +38,20 @@ export function SortableItem(props) {
   );
 }
 
-const MultiValueContainer = (props: MultiValueGenericProps<AuthorOption>) => {
-
+const MultiValue = (props: MultiValueProps<AuthorOption, true, GroupBase<AuthorOption>>) => {
   return (
     <SortableItem key={props.data.id} id={props.data.id}>
-      <components.MultiValueContainer {...props} />
+      <components.MultiValue {...props} />
     </SortableItem>
   );
 };
 
-
-function SingleValue(props) {
-  return (
-    <SortableItem key={props.data.id} id={props.data.id}>
-      <components.SingleValue {...props} />
-    </SortableItem>
-  )
-}
-
 const MultiValueRemove = (props: MultiValueRemoveProps<AuthorOption>) => {
-  const handleOnMouseDown = (e : React.MouseEvent<HTMLDivElement>) => {
 
-    props.selectProps.onSetValues((authors : AuthorOption[]) => {
-      var newAuthors = authors.filter(author => author.id != props.data.id);
-
-      return newAuthors;
-    })
-
-    e.preventDefault();
-    e.stopPropagation();
-  }
+  props.innerProps.onMouseDown = props.innerProps.onClick;
 
   return (
-    <>
-      <components.MultiValueRemove {...props}>
-        <div onMouseDown={handleOnMouseDown}>X</div>
-      </components.MultiValueRemove>
-    </>
+    <components.MultiValueRemove {...props} />
   );
 };
 
@@ -95,8 +72,11 @@ const DroppableValueContainer = ({ children, ...props }: ValueContainerProps<Aut
         const oldIndex = authors.findIndex(author => author.id == active.id);
         const newIndex = authors.findIndex(author => author.id == over.id);
 
-        var result = arrayMove(authors, oldIndex, newIndex);
-        return result;
+        if (oldIndex == -1 || newIndex == -1) {
+          return [... authors];
+        } else {
+          return arrayMove(authors, oldIndex, newIndex);
+        }
       });
     }
   }
@@ -120,28 +100,28 @@ export function Author({ list }: { list: string[] }) {
   var authorOptions = list.sort()
     .map((author, index) => ({ id: `${index}`, label: author, value: author }));
 
-    const handleOnChange = (selectedOptions: OnChangeValue<AuthorOption, true>) => {
-      if (selectedOptions == null) {
-        selectedOptions = [];
-      }
-  
-      selectedOptions.forEach(option => {
-        if (option.id == null) {
-          option.id = "" + Math.random().toString(36).substring(2, 9);
-        }
-      })
-  
-      setAuthors([...selectedOptions]);
+  const handleOnChange = (selectedOptions: OnChangeValue<AuthorOption, true>) => {
+    if (selectedOptions == null) {
+      selectedOptions = [];
     }
 
-    return (
+    selectedOptions.forEach(option => {
+      if (option.id == null) {
+        option.id = "" + Math.random().toString(36).substring(2, 9);
+      }
+    })
+
+    setAuthors(_authors => [...selectedOptions]);
+  }
+
+  return (
     <div className="flex">
       <div className="w-1/6">
         <label htmlFor="author">Author</label>
       </div>
       <div id="author_container" className="w-5/6">
         <CreateableSelect
-          components={{ MultiValueContainer, ValueContainer: DroppableValueContainer, MultiValueRemove, SingleValue }}
+          components={{ MultiValue, MultiValueRemove, ValueContainer: DroppableValueContainer }}
           instanceId="author-select"
           options={authorOptions}
           isClearable
