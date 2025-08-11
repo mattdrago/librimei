83f23d136daa7af134c2a64139bf594abb969b5c
Matt Drago <matt@drago.com.au>
2025-08-11 21:32:16 +1000

    Distribute: adds scripts to deploy libri-mei into an LXC

---
+++ lxc-dist.sh
@@ -0,0 +1,25 @@
+#!/bin/ash
+
+APP_DIR=/libri-mei
+
+rm -rf "${APP_DIR}"
+
+mkdir -p "${APP_DIR}"
+
+cp -R ./public "${APP_DIR}"
+cp -R ./.next/standalone/* "${APP_DIR}"
+cp -R ./.next/standalone/.next "${APP_DIR}"
+cp -R ./.next/static "${APP_DIR}/.next"
+
+echo "NODE_ENV=production" > "${APP_DIR}/.env"
+echo "NEXT_TELEMETRY_DISABLED=1" >> "${APP_DIR}/.env"
+echo "PORT=80" >> "${APP_DIR}/.env"
+echo "HOSTNAME='0.0.0.0'" >> "${APP_DIR}/.env"
+echo "LIBRARY_FOLDER=/elibrary" >> "${APP_DIR}/.env"
+
+cp ./lxc-initd.alpine /etc/init.d/libri-mei
+mkdir -p /run/libri-mei
+
+rc-update add libri-mei
+rc-service libri-mei stop
+rc-service libri-mei start
