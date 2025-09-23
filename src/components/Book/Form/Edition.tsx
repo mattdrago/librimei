e20ad8f377491e82904b86b7219e692bf489e757
Matt Drago <matt@drago.com.au>
2025-09-23 17:17:14 +1000

    Add Book: extract most frequent isbn from first 10 pages of pdf

--- src/components/Book/Form/Edition.tsx
+++ src/components/Book/Form/Edition.tsx
@@ -1,41 +1,21 @@
 "use client";
 
 import { ChangeEvent, useEffect, useState } from "react";
+import ISBN from "isbn3"
 
 export function Edition() {
 
-  const [fileContent, setFileContent] = useState<string | ArrayBuffer | null>(null);
-
   function fileChanged(e: ChangeEvent<HTMLInputElement>) {
     if (!e.target.files || e.target.files.length == 0) {
-      console.log("no File input");
       return;
     }
+    const file = e.target.files[0];
 
     const r = new FileReader();
-    r.onload = () => { console.log(r.result); setFileContent(r.result)};
-    r.onerror = () => { console.log("Error reading file")};
-    r.readAsArrayBuffer(e.target.files[0]);
+    r.onload = async () => { const isbn = await extractIsbn(r.result, file.type); console.log(isbn); };
+    r.onerror = () => { console.log("Error reading file") };
+    r.readAsArrayBuffer(file);
   }
-
-  useEffect(() => {
-    async function extractIsbn() {
-      if (fileContent == null) {
-        return;
-      }
-
-      const pdfjs = window.pdfjsLib as typeof import('pdfjs-dist/types/src/pdf');
-      pdfjs.GlobalWorkerOptions.workerSrc = '/pdfjs/pdf.worker.min.mjs';
-  
-      const pdfDocument = await pdfjs.getDocument(fileContent).promise
-  
-      console.log('pdfDocument', pdfDocument);
-    }
-
-    extractIsbn();
-    return () => {
-    };
-  }, [fileContent]);
 
   return (
     <div className="flex">
@@ -51,8 +31,73 @@ export function Edition() {
           placeholder="Edition"
           className="border w-full"
           onChange={fileChanged}
+          accept="application/pdf"
         />
       </div>
     </div>
   );
+}
+
+
+async function extractIsbn(fileContent: string | ArrayBuffer | null, contentType: string): Promise<string | null> {
+  if (fileContent == null) {
+    return null;
+  }
+
+  const pages = getPdfPages(fileContent);
+  const foundIsbnCounts: Map<string, number> = new Map<string, number>();
+
+  let pageContent : IteratorResult<string, string>;
+  for(let pageCount = 0; (pageContent = await pages.next()) && pageCount < 10; ++pageCount) {
+    const isbnMatch = pageContent.value.match(/((978[-– ])?[0-9][0-9-– ]{10}[-– ][0-9xX])|((978)?[0-9]{9}[0-9Xx])/);
+
+    if (isbnMatch) {
+      isbnMatch.forEach(str => {
+        const isbn = ISBN.asIsbn13(str);
+        if (isbn) {
+          let count = foundIsbnCounts.get(isbn) ?? 0;
+          foundIsbnCounts.set(isbn, count + 1);
+        }
+      })
+    }
+  }
+
+  return getMostFrequentIsbn(foundIsbnCounts);
+}
+
+async function* getPdfPages(content: string | ArrayBuffer ) : AsyncGenerator<string, string, string> {
+  const pdfjs = window.pdfjsLib as typeof import('pdfjs-dist/types/src/pdf');
+  pdfjs.GlobalWorkerOptions.workerSrc = '/pdfjs/pdf.worker.min.mjs';
+
+  const pdfDocument = await pdfjs.getDocument(content).promise
+
+  for (let pageNum = 1; pageNum <= pdfDocument.numPages; pageNum++) {
+    const page = await pdfDocument.getPage(pageNum);
+    const pageContent = await page.getTextContent();
+
+    let fullPageContent = pageContent.items.map(contentItem => {
+      if ('str' in contentItem) {
+        return contentItem.str;
+      }
+
+      return '';
+    }).join(' ');
+
+    yield fullPageContent;
+  }
+
+  return '';
+}
+
+function getMostFrequentIsbn(isbnCounts : Map<string, number>) : string | null {
+  let isbn = null
+  let isbnCount = 0;
+  isbnCounts.forEach((currIsbnCount, currIsbn) => {
+    if (currIsbnCount > isbnCount) {
+      isbn = currIsbn;
+      isbnCount = currIsbnCount;
+    }
+  });
+
+  return isbn;
 }
