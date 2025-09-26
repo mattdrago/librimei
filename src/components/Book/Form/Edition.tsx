22379e96cae09ef253bcd6a4ce6d15b91b294506
Matt Drago <matt@drago.com.au>
2025-09-26 14:25:45 +1000

    Add Book: extract most frequent isbn from first 10 content pieces of epub

--- src/components/Book/Form/Edition.tsx
+++ src/components/Book/Form/Edition.tsx
@@ -39,49 +39,36 @@ export function Edition() {
   );
 }
 
+interface ContentGenerator {
+  (content: string | ArrayBuffer ) : AsyncGenerator<string, string, string>,
+}
 
 async function extractIsbn(fileContent: string | ArrayBuffer | null, contentType: string): Promise<string | null> {
   if (fileContent == null) {
     return null;
   }
 
+  let contentFunction: ContentGenerator;
+
   switch (contentType) {
     case 'application/pdf':
-      return extractIsbnFromPdf(fileContent);
+      return extractIsbnFrom(getPdfText(fileContent));
 
     case 'application/epub+zip':
-      return extractIsbnFromEpub(fileContent);
+      return extractIsbnFrom(getEpubText(fileContent));
   
     default:
       return null;
   }
 }
 
-async function extractIsbnFromEpub(fileContent: string | ArrayBuffer): Promise<string | null> {
+async function extractIsbnFrom(contentGenerator: AsyncGenerator<string, string, string>): Promise<string | null> {
 
-  const buf = (typeof fileContent == "string") ? Buffer.from(fileContent) : Buffer.from(fileContent);
-  // @ts-expect-error EPub pass the filename to the adm-zip which can handle filename being a Buffer
-  const epub = new EPub(buf, '', '');
-
-  const promise = new Promise<string | null>((resolve, reject) => {
-    // ISBN is not in the metadata as a standard.  Need to do the content extract approach like pdf.
-    epub.on('end', () => resolve(epub.metadata.title));
-    epub.on('error', () => reject("Error loading epub"));
-  });
-  
-  epub.parse();
-
-  return promise;
-}
-
-async function extractIsbnFromPdf(fileContent: string | ArrayBuffer): Promise<string | null> {
-
-  const pages = getPdfPages(fileContent);
   const foundIsbnCounts: Map<string, number> = new Map<string, number>();
 
-  let pageContent : IteratorResult<string, string>;
-  for(let pageCount = 0; (pageContent = await pages.next()) && pageCount < 10; ++pageCount) {
-    const isbnMatch = pageContent.value.match(/((978[-– ])?[0-9][0-9-– ]{10}[-– ][0-9xX])|((978)?[0-9]{9}[0-9Xx])/);
+  let content : IteratorResult<string, string>;
+  for(let contentCount = 0; (content = await contentGenerator.next()) && contentCount < 10; ++contentCount) {
+    const isbnMatch = content.value.match(/((978[-– ])?[0-9][0-9-– ]{10}[-– ][0-9xX])|((978)?[0-9]{9}[0-9Xx])/);
 
     if (isbnMatch) {
       isbnMatch.forEach(str => {
@@ -97,7 +84,40 @@ async function extractIsbnFromPdf(fileContent: string | ArrayBuffer): Promise<st
   return getMostFrequentIsbn(foundIsbnCounts);
 }
 
-async function* getPdfPages(content: string | ArrayBuffer ) : AsyncGenerator<string, string, string> {
+async function* getEpubText(content: string | ArrayBuffer ) : AsyncGenerator<string, string, string> {
+  const buf = (typeof content == "string") ? Buffer.from(content) : Buffer.from(content);
+  // @ts-expect-error EPub pass the filename to the adm-zip which can handle filename being a Buffer
+  const epub = new EPub(buf, '', '');
+
+  const promise = new Promise<void>((resolve, reject) => {
+    epub.on('end', () => resolve());
+    epub.on('error', () => reject("Error loading epub"));
+  });
+  
+  epub.parse();
+
+  await promise;
+
+  for(let chapterIndex = 0; chapterIndex < epub.flow.length; ++chapterIndex) {
+
+    const promise = new Promise<string>((resolve, reject) => {
+      epub.getChapterRaw(epub.flow[chapterIndex].id, (error, text) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(text);
+        }
+      })
+    });
+
+    yield await promise;
+  }
+
+  return '';
+}
+
+
+async function* getPdfText(content: string | ArrayBuffer ) : AsyncGenerator<string, string, string> {
   // @ts-expect-error pdfjsLib is added to window with the <script> tag
   const pdfjs = window.pdfjsLib as typeof import('pdfjs-dist/types/src/pdf');
   pdfjs.GlobalWorkerOptions.workerSrc = '/pdfjs/pdf.worker.min.mjs';
